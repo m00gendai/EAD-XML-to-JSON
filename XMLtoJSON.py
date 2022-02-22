830abea1505be71656ddfb0d6fb648d0ec7d81d7
Marcel Weber <74364298+m00gendai@users.noreply.github.com>
2022-02-22 19:01:07 +0100

    Lightly expanded for functionality

--- XMLtoJSON.py
+++ XMLtoJSON.py
@@ -1,4 +1,4 @@
-# Program to convert an xml 
+# Program to convert an xml
 # file to json file
 # source https://www.geeksforgeeks.org/python-xml-to-json/
 # and
@@ -9,10 +9,13 @@ import xmltodict
  
 ad_files = ["EAD_AD_AFR", "EAD_AD_ASIA", "EAD_AD_EUR", "EAD_AD_PANAM"]
 navaid_files = ["EAD_NAV_DME", "EAD_NAV_TACAN", "EAD_NAV_VOR"]
+waypoint_files = ["EAD_WPT_NE", "EAD_WPT_NW", "EAD_WPT_SE"]
 categories = [
-    [ad_files, "EAD_AD_ALL"],
-    [navaid_files, "EAD_NAV_ALL"]
-    ]
+    [ad_files, "EAD_AD_ALL", "airports"],
+    [navaid_files, "EAD_NAV_ALL", "navaids"],
+    [waypoint_files, "EAD_WPT_ALL", "waypoints"]
+]
+
 
 def parseXML(xml_files):
     counter = 0
@@ -32,22 +35,52 @@ def parseXML(xml_files):
                 counter += 1
     print(f"Successfully converted {counter} XML files to JSON")
 
-def mergeJSON(json_files, name):
+
+def mergeJSON(json_files, name, const):
     counter = 0
     result = list()
     print(f"Start merging {len(json_files)} JSON files")
     for file in json_files:
         print(f"Processing {file}.json")
         with open(f"{file}.json", 'r') as infile:
-            result.append(json.load(infile))
-            counter+= 1
-            
-    with open(f"{name}.json", 'w') as output_file:
-        json.dump([result], output_file)
-    print(f"Successfully merged {counter} JSON files into {name}.json")
+            json_data = json.load(infile)
+            for record in json_data["SdoReportResponse"]["SdoReportResult"]["Record"]:
+                if name == "EAD_WPT_ALL":
+                    if record["codeType"] == "ICAO":
+                        item = dict(codeId=record["codeId"], geoLat=record["geoLat"], geoLong=record["geoLong"])
+                        result.append(item)
+                if name == "EAD_NAV_ALL":
+                    if "codeType" in record:
+                        if "txtName" in record:
+                            print(record["txtName"])
+                            item = dict(codeId=record["codeId"], geoLat=record["geoLat"], geoLong=record["geoLong"], 
+                                        txtName=record["txtName"], codeType=record["codeType"])
+                            result.append(item)
+                        else:
+                            item = dict(codeId=record["codeId"], geoLat=record["geoLat"], geoLong=record["geoLong"], 
+                                        txtName="", codeType=record["codeType"])
+                            result.append(item)
+                    else:
+                        if "txtName" in record:
+                            item = dict(codeId=record["codeId"], geoLat=record["geoLat"], geoLong=record["geoLong"], 
+                                        txtName=record["txtName"], codeType="DME or TACAN")
+                            result.append(item)
+                        else:
+                            item = dict(codeId=record["codeId"], geoLat=record["geoLat"], geoLong=record["geoLong"], 
+                                        txtName="", codeType="DME or TACAN")
+                            result.append(item)
+                if name == "EAD_AD_ALL":
+                    item = dict(codeId=record["codeId"], geoLat=record["geoLat"], geoLong=record["geoLong"], 
+                                txtName=record["txtName"])
+                    result.append(item)
+        counter += 1
+    with open(f"{name}.js", 'w') as output_file:
+        output_file.write(f"const {const} = {result}")
+        print(f"Successfully merged {counter} JSON files into {name}.js")
+
 
 for category in categories:
     parseXML(category[0])
-    mergeJSON(category[0], category[1])
+    mergeJSON(category[0], category[1], category[2])
 
 input("ENTER to exit")
